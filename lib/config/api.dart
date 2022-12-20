4aa141cb2895651dcd7ce6c25e04c9652e5ff1d3
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-12-21 06:27:38 +0700

    added api

---
+++ lib/config/api.dart
@@ -0,0 +1,11 @@
+class Api {
+  static const _baseUrl = "http://192.168.100.248/api_chat_app";
+  static const comment = "$_baseUrl/comment";
+  static const follow = "$_baseUrl/follow";
+  static const topic = "$_baseUrl/topic";
+  static const user = "$_baseUrl/user";
+
+  static const userImage = "$_baseUrl/image/user";
+  static const commentImage = "$_baseUrl/image/comment";
+  static const topicImage = "$_baseUrl/image/topic";
+}
