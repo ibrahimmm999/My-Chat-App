54ca93f3b550c19c4eda42dcc0fa90225a6468c7
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-12-28 12:58:53 +0700

    added register backend

---
+++ lib/source/user_source.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:chat_app/config/api.dart';
+import 'package:d_method/d_method.dart';
+import 'package:http/http.dart' as http;
+
+class UserSource {
+  register(String username, String password) async {
+    String url = '${Api.user}/register.php';
+    try {
+      http.Response response = await http.post(Uri.parse(url), body: {
+        'username': username,
+        'password': password,
+      });
+      DMethod.printTitle('User source - register', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User source - register', e.toString());
+      return {'success': false};
+    }
+  }
+}
