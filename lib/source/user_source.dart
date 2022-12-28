a714ae3cc57e9c3daa661ef236a2e02524872539
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-12-28 14:23:05 +0700

    added login method

--- lib/source/user_source.dart
+++ lib/source/user_source.dart
@@ -5,7 +5,7 @@ import 'package:d_method/d_method.dart';
 import 'package:http/http.dart' as http;
 
 class UserSource {
-  register(String username, String password) async {
+  static Future<Map> register(String username, String password) async {
     String url = '${Api.user}/register.php';
     try {
       http.Response response = await http.post(Uri.parse(url), body: {
@@ -17,6 +17,22 @@ class UserSource {
       return responseBody;
     } catch (e) {
       DMethod.printTitle('User source - register', e.toString());
+      return {'success': false};
+    }
+  }
+
+  static Future<Map> login(String username, String password) async {
+    String url = '${Api.user}/login.php';
+    try {
+      http.Response response = await http.post(Uri.parse(url), body: {
+        'username': username,
+        'password': password,
+      });
+      DMethod.printTitle('User source - login', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User source - login', e.toString());
       return {'success': false};
     }
   }
